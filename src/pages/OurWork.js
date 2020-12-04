d7b5edb91bd68ffbc5919a95deb6d646e720aee7
ErwinWoz <eerwoz@gmail.com>
2020-12-04 07:01:52 -0800

    styling our work page

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -1,11 +1,60 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+//Images
+import athlete from '../img/athlete-small.png';
+import theracer from '../img/theracer-small.png';
+import goodtimes from '../img/goodtimes-small.png';
 
 const OurWork = () => {
     return ( 
-        <div>
-            <h1>Out Work</h1>
-        </div>
+        <OurWorkStyle>
+            <MovieStyle>
+                <h2>The Athlete</h2>
+                <div className="line"></div>
+                <Link>
+                    <img src={athlete} alt="athlete"/>
+                </Link>
+            </MovieStyle>
+            <MovieStyle>
+                <h2>The Racer</h2>
+                <div className="line"></div>
+                <Link>
+                    <img src={theracer} alt="theracer"/>
+                </Link>
+            </MovieStyle>
+            <MovieStyle>
+                <h2>Good Times</h2>
+                <div className="line"></div>
+                <Link>
+                    <img src={goodtimes} alt="goodtimes"/>
+                </Link>
+            </MovieStyle>
+        </OurWorkStyle>
      );
 };
+
+const OurWorkStyle = styled.div`
+    min-height: 100vh;
+    overflow: hidden;
+    padding: 5rem 10rem;
+    h2 {
+        padding: 1rem 0rem;
+    }
+`;
  
+const MovieStyle = styled.div`
+    padding-bottom: 10rem;
+    .line {
+        height: 0.5rem;
+        background-color: #ccc;
+        margin-bottom: 3rem;
+    }
+    img {
+        width: 100%;
+        height: 70vh;
+        object-fit: cover;
+    }
+`;
+
 export default OurWork;
